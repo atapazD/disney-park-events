470dda5901fa322049cfe22047a7596e69ec954b
atapazD <daniel.zapata1011@gmail.com>
2025-04-03 08:58:53 -0400

    update host port

--- backend/app.py
+++ backend/app.py
@@ -67,4 +67,4 @@ def book_event():
     return jsonify({"success": False, "message": "Event not found"}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
